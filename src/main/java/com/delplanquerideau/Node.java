a2d5efab47471b305a427a17b6d94c2adb8d1920
Delplanque Nicolas <delplanque.n@outlook.com>
2022-11-08 10:35:36 +0100

    Add start and end of X & Y

--- src/main/java/com/delplanquerideau/Node.java
+++ src/main/java/com/delplanquerideau/Node.java
@@ -6,6 +6,10 @@ package com.delplanquerideau;
 public class Node {
     private CuttingDirection cuttingDirection;
     private Colors color;
+    private int startX;
+    private int endX;
+    private int startY;
+    private int endY;
     private Node leftNode;
     private Node rightNode;
 
@@ -16,9 +20,13 @@ public class Node {
      * @param leftNode
      * @param rightNode
      */
-    public Node(CuttingDirection cuttingDirection, Colors color, Node leftNode, Node rightNode){
+    public Node(CuttingDirection cuttingDirection, Colors color, int startX, int endX, int startY, int endY, Node leftNode, Node rightNode){
         this.cuttingDirection = cuttingDirection;
         this.color = color;
+        this.startX = startX;
+        this.endX = endX;
+        this.startY = startY;
+        this.endY = endY;
         this.leftNode = leftNode;
         this.rightNode = rightNode;
     }
@@ -28,9 +36,13 @@ public class Node {
      * @param cuttingDirection
      * @param color
      */
-    public Node(CuttingDirection cuttingDirection, Colors color){
+    public Node(CuttingDirection cuttingDirection, Colors color, int startX, int endX, int startY, int endY){
         this.cuttingDirection = cuttingDirection;
         this.color = color;
+        this.startX = startX;
+        this.endX = endX;
+        this.startY = startY;
+        this.endY = endY;
         this.leftNode = null;
         this.rightNode = null;
     }
@@ -49,5 +61,18 @@ public class Node {
 
     public Node getLeftNode(){
         return leftNode;
+    }
+    
+    /**
+     * Calcul le poids d'une feuille
+     * @param w la largeur
+     * @param h la hauteur
+     * @return
+     */
+    public double weight(){
+        int w = this.endX - this.startX;
+        int h = this.endY - this.startY;
+        
+        return (w*h)/(w+h)*1.5*1.5;
     }
 }
