faa8d9c0bb4b026412e4682a83003c5d0f9f997f
Delplanque Nicolas <delplanque.n@outlook.com>
2022-11-08 10:53:44 +0100

    Add cut function and 2 more params in constructor

--- src/main/java/com/delplanquerideau/TwoDimensionalTree.java
+++ src/main/java/com/delplanquerideau/TwoDimensionalTree.java
@@ -5,8 +5,18 @@ package com.delplanquerideau;
  */
 public class TwoDimensionalTree {
 
-    public TwoDimensionalTree(){
-
+    public TwoDimensionalTree(int largeur, int hauteur, int nbFeuilles, float proportionCoupe, int minDimensionCoupe, float memeCouleurProb, int largeurLigne){
+        
     }
 
+
+    // public void cut(){
+    //     if(this.rightNode == null && this.leftNode == null){
+    //         if(this.cuttingDirection == CuttingDirection.X){
+                
+    //         }else if(this.cuttingDirection == CuttingDirection.Y){
+
+    //         }
+    //     }
+    // }
 }
