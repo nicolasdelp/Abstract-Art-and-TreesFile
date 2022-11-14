373c97123d5f90d270f882cb0a6cc616f2c6c7b4
Delplanque Nicolas <delplanque.n@outlook.com>
2022-11-14 18:05:39 +0100

    Enum to a Class

--- src/main/java/com/delplanquerideau/CuttingDirection.java
+++ src/main/java/com/delplanquerideau/CuttingDirection.java
@@ -1,3 +1,25 @@
 package com.delplanquerideau;
 
-public enum CuttingDirection {X, Y}
+public class CuttingDirection{
+    private Direction direction;
+    private int x;
+    private int y;
+
+    public CuttingDirection(Direction direction, int x, int y){
+        this.direction = direction;
+        this.x = x;
+        this.y = y;
+    }
+
+    public Direction getDirection() {
+        return direction;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
