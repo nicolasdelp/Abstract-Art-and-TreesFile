915cc32d0c34036d3fe029a4e817e6c594906edd
Delplanque Nicolas <delplanque.n@outlook.com>
2022-11-14 18:06:26 +0100

    Class to generate tree

---
+++ src/main/java/com/delplanquerideau/GenerateTree.java
@@ -0,0 +1,11 @@
+package com.delplanquerideau;
+
+public class GenerateTree {
+    public TwoDimensionalTree generateRandomTree(int largeur, int hauteur, int nbFeuilles, float proportionCoupe, int minDimensionCoupe, float memeCouleurProb, int largeurLigne, int seed){
+        return null;
+    }
+    
+    public TwoDimensionalTree generateBetterRandomTree(){
+        return null;
+    }
+}
