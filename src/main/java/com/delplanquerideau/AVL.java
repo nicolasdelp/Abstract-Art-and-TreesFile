7065ea800bfa4c2af69e94cd637a77e3c4af8351
Delplanque Nicolas <delplanque.n@outlook.com>
2022-11-25 17:30:38 +0100

    AVL terminé

--- src/main/java/com/delplanquerideau/AVL.java
+++ src/main/java/com/delplanquerideau/AVL.java
@@ -1,91 +1,207 @@
-// package com.delplanquerideau;
+package com.delplanquerideau;
 
-// public class AVL {
-//     Node root;
+public class AVL {
+
+    private Node racine;
+
+    public AVL(Node n){
+        this.racine = n;
+    }
+
+    public int getNodeHeight(Node n){
+        if(n == null){
+            return -1;
+        }
+        return n.getAVLHeight();
+    }
+
+    public void updateNodeHeight(Node n){
+        n.setAVLHeight(Math.max(getNodeHeight(n.getLeftNode()), getNodeHeight(n.getRightNode())) + 1); //On prend le max des deux sous-arbres et on ajoute 1
+    }
+
+    public int getNodeBalance(Node n){
+        if(n == null){
+            return 0;
+        }
+        return getNodeHeight(n.getRightNode()) - getNodeHeight(n.getLeftNode()); // Différence entre la hauteur des deux sous-arbres
+    }
+
+    public Node rightRotation(Node n){
+        Node a = n.getLeftNode();
+        Node b = a.getRightNode();
+        a.setRightNode(n);
+        n.setLeftNode(b);
+        updateNodeHeight(n);
+        updateNodeHeight(a);
+        return a;
+    }
+
+    public Node leftRotation(Node n){
+        Node a = n.getRightNode();
+        Node b = a.getLeftNode();
+        a.setLeftNode(n);
+        n.setRightNode(b);
+        updateNodeHeight(n);
+        updateNodeHeight(a);
+        return a;
+    }
+
+    public Node balance(Node n){
+        updateNodeHeight(n);
+
+        if(getNodeBalance(n) > 1){
+            if(getNodeHeight(n.getRightNode().getRightNode()) > getNodeHeight(n.getRightNode().getLeftNode())){
+                n = leftRotation(n);
+            }else {
+                n.setRightNode(rightRotation(n.getRightNode()));
+                n = leftRotation(n);
+            }
+        }else if(getNodeBalance(n) < -1){
+            if(getNodeHeight(n.getLeftNode().getLeftNode()) > getNodeHeight(n.getLeftNode().getRightNode())){
+                n = rightRotation(n);
+            }else {
+                n.setLeftNode(leftRotation(n.getLeftNode()));
+                n = rightRotation(n);
+            }
+        }
+        return n;
+    }
+
+    public Node insert(Node r, Node n){
+        double nodeWeight = n.weight();
+
+        if(r == null){
+            return r;
+        }
+        if(r.weight() > nodeWeight){
+            if(r.getLeftNode() != null){
+                r.setLeftNode(insert(r.getLeftNode(), n));
+            }else{
+                r.setLeftNode(n);
+            }
+        }else if(r.weight() > nodeWeight){
+            if(r.getRightNode() != null){
+                r.setRightNode(insert(r.getRightNode(), n));
+            }else{
+                r.setRightNode(n);
+            }
+        }
+        return balance(r);
+    }
+
+    public Node delete(Node r, Node n){
+        double nodeWeight = n.weight();
+
+        if(r == null){
+            return r;
+        }
+        if(r.weight() > nodeWeight){
+            r.setLeftNode(delete(r.getLeftNode(), n));
+        }else if(r.weight() < nodeWeight){
+            r.setRightNode(delete(r.getRightNode(), n));
+        }else{ // Si c'est le noeud a supprimer
+            if(r.getLeftNode() == null){
+                r = r.getRightNode();
+            }else if(r.getRightNode() == null){
+                r = r.getLeftNode();
+            }else{
+                // On copie le noeud à la racine
+                Node minimum = getMin(r.getRightNode());
+                r.setCuttingDirection(minimum.getCuttingDirection());
+                r.setColor(minimum.getColor());
+                r.setStartX(minimum.getStartX());
+                r.setEndX(minimum.getEndX());
+                r.setStartY(minimum.getStartY());
+                r.setEndY(minimum.getEndX());
+
+                // On supprime le noeud qu'on vient de copier
+                r.setRightNode(delete(r.getRightNode(), minimum));
+            }
+        }
+        if(r != null){
+            r = balance(r);
+        }
+        return r;
+    }
+
+    public Node getMax(Node n){
+        if(n.getRightNode() != null) {
+            return getMax(n.getRightNode()); // On veut le noeud le plus à droite de l'arbre
+        }else{
+            return n;
+        }
+    }
+
+    public Node getMin(Node n){
+        if(n.getLeftNode() != null) {
+            return getMax(n.getLeftNode()); // On veut le noeud le plus à gauche de l'arbre
+        }else{
+            return n;
+        }
+    }
+
+    
+
+    // public String preorder(Node n, String s){
+    //     s = s + n.toString();
+    //     if(!n.getIsALeaf()){
+    //         s = s + "(" + preorder(n.getLeftNode(), s) + ")";
+    //         s = s + "(" + preorder(n.getRightNode(), s) + ")";
+    //     }
+    //     return s;
+    // }
+
+    // public Node insert(Node n) {
 
 
+    //     public Node insert_rec (Node curr, Node toAdd){
+    //         if (curr == null) {
+    //             return toAdd;    //we get the existing node (from the Kd Tree)
+    //         } else if (toAdd.getWeight() <= curr.getWeight()) {    //if equal, insert node as child, choice for next division is left to fate
+    //             curr.setLeftNode(insert_rec(curr.getLeftNode(), toAdd));
+    //         } else {
+    //             curr.setRightNode(insert_rec(curr.getRightNode(), toAdd));
+    //         }
 
-//     public AVL(){
-//     }
-
-//     public AVL(Node n){
-//         Node root = new Node(n);  //todo : create node constructor from node
-//     }
-
-
-//     public boolean isEmpty(){
-//         return (this.root = null);
-//     }
-
-
-//     public String preorder(Node n, String s){
-//         s = s + n.toString();
-//         if(!n.getIsALeaf()){
-//             s = s + "(" + preorder(n.getLeftNode(), s) + ")";
-//             s = s + "(" + preorder(n.getRightNode(), s) + ")";
-//         }
-//         return s;
-//     }
-
-//     public Node insert(Node n){
-//         insert_rec(root,n);
-
-//         public Node insert_rec(Node curr, Node toAdd){
-//             if(curr==null){
-//                 return toAdd;	//we get the existing node (from the Kd Tree)
-//             }
-//             else if(toAdd.getWeight() <= curr.getWeight()){	//if equal, insert node as child, choice for next division is left to fate
-//                 curr.setLeftNode(insert_rec(curr.getLeftNode(), toAdd));
-//             }
-//             else{
-//                 curr.setRightNode(insert_rec(curr.getRightNode(), toAdd)) ;
-//             }
-
-//             updateHeight(n);
-//             rotate(n);
-//             return Node;
+    //         updateHeight(n);
+    //         rotate(n);
+    //         return Node;
 
 
+    //         // //override
+    //         // public Node delete(Node toDel){
+    //         // 	delete_rec(curr, root);
+    //         // }
+
+    //         // public Node delete_rec(Node curr, Node toDel){	//find method is useless, delete wants to have the parents of the node to delete
+    //         // 	if(n==null) return null;
+    //         // 	else if(toAdd.getWeight() < curr.getWeight()){	//if equal, delete ? how to know which to delete? is deletion necessary here?
+    //         // 		curr.setLeftNode(delete_rec(curr.getLeftNode(), toAdd));
+    //         // 	}
+    //         // 	else{
+    //         // 		curr.setRightNode(delete_rec(curr.getRightNode(), toAdd)) ;
+    //         // 	}
+
+    //         // }
 
 
+    //         public Node getMax () {
+    //             Node n = root;
+    //             while (!n.isLeaf) {
+    //                 n = n.getRightNode;
+    //             }
+    //             return n;
+    //         }
 
+    //         public Node getMin () {
+    //             Node n = root;
+    //             while (!n.isLeaf) {
+    //                 n = n.getLeftNode;
+    //             }
+    //             return n;
+    //         }
+    //     }
 
-
-
-
-//             // //override
-//             // public Node delete(Node toDel){
-//             // 	delete_rec(curr, root);
-//             // }
-
-//             // public Node delete_rec(Node curr, Node toDel){	//find method is useless, delete wants to have the parents of the node to delete
-//             // 	if(n==null) return null;
-//             // 	else if(toAdd.getWeight() < curr.getWeight()){	//if equal, delete ? how to know which to delete? is deletion necessary here?
-//             // 		curr.setLeftNode(delete_rec(curr.getLeftNode(), toAdd));
-//             // 	}
-//             // 	else{
-//             // 		curr.setRightNode(delete_rec(curr.getRightNode(), toAdd)) ;
-//             // 	}
-
-//             // }
-
-
-
-//             public Node getMax(){
-//                 Node n = root;
-//                 while(!n.isLeaf){
-//                     n = n.getRightNode;
-//                 }
-//                 return n;
-//             }
-
-//             public Node getMin(){
-//                 Node n = root;
-//                 while(!n.isLeaf){
-//                     n = n.getLeftNode;
-//                 }
-//                 return n;
-//             }
-
-
-//         }
+    //     insert_rec(root, n);
+    // }
+}
